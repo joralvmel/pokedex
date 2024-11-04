587e9fe3bc34384033cb6812407a850d1f4082ed
joralvmel <joralvmel@gmail.com>
2024-11-04 12:05:52 +0100

    move options to drawer menu

--- app/src/main/java/es/upm/miw/pokedex/ui/fragment/PokedexFragment.java
+++ app/src/main/java/es/upm/miw/pokedex/ui/fragment/PokedexFragment.java
@@ -2,20 +2,24 @@ package es.upm.miw.pokedex.ui.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.android.material.navigation.NavigationView;
 
 import es.upm.miw.pokedex.R;
 import es.upm.miw.pokedex.ui.viewmodel.PokemonViewModel;
@@ -23,7 +27,8 @@ import es.upm.miw.pokedex.ui.viewmodel.PokemonViewModel;
 public class PokedexFragment extends Fragment {
 
     private FirebaseAuth mAuth;
-    private TextView statusTextView;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Nullable
     @Override
@@ -33,17 +38,18 @@ public class PokedexFragment extends Fragment {
         // Initialize FirebaseAuth
         mAuth = FirebaseAuth.getInstance();
 
-        // Initialize UI elements
-        statusTextView = view.findViewById(R.id.statusTextView);
-        Button signOutButton = view.findViewById(R.id.signOutButton);
-        Button reloadButton = view.findViewById(R.id.reload_button);
+        // Initialize DrawerLayout and NavigationView
+        drawerLayout = view.findViewById(R.id.drawer_layout);
+        navigationView = view.findViewById(R.id.navigation_view);
 
-        // Configure buttons
-        signOutButton.setOnClickListener(v -> signOut());
-        reloadButton.setOnClickListener(v -> {
-            PokemonViewModel viewModel = new ViewModelProvider(this).get(PokemonViewModel.class);
-            viewModel.fetchAllPokemon();
-        });
+        // Initialize Toolbar
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(getActivity(), drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Configure NavigationView
+        navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
 
         // Check current user and update UI
         FirebaseUser currentUser = mAuth.getCurrentUser();
@@ -56,11 +62,13 @@ public class PokedexFragment extends Fragment {
     }
 
     private void updateUI(FirebaseUser user) {
+        Menu menu = navigationView.getMenu();
+        MenuItem currentUserItem = menu.findItem(R.id.menu_current_user);
         if (user != null) {
             String email = user.getEmail();
-            statusTextView.setText(email);
+            currentUserItem.setTitle(email);
         } else {
-            statusTextView.setText(R.string.not_authenticated);
+            currentUserItem.setTitle(R.string.current_user);
         }
     }
 
@@ -81,5 +89,17 @@ public class PokedexFragment extends Fragment {
         FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
         transaction.replace(R.id.pokemon_list_container, pokemonListFragment);
         transaction.commit();
+    }
+
+    private boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.menu_sign_out) {
+            signOut();
+        } else if (id == R.id.menu_reload) {
+            PokemonViewModel viewModel = new ViewModelProvider(this).get(PokemonViewModel.class);
+            viewModel.fetchAllPokemon();
+        }
+        drawerLayout.closeDrawer(navigationView);
+        return true;
     }
 }
